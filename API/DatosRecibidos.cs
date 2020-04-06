834cf2c2fb033abc6163fb720791c8c20f458bd7
Alejandra Samayoa <ale.samayoa@outlook.es>
2020-04-06 00:03:05 -0600

    terminado

---
+++ API/DatosRecibidos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class DatosRecibidos
+    {
+        public string Path      { get; set; }
+        public string Name          { get; set; }
+        public int   CesarKey           { get; set; }
+        public int   ZigZag_Grade           { get; set; }
+        public int    m                 { get; set; }
+        public string  Read             {get;set;}
+        public string  Write            { get; set; }
+    }
+}
---
+++ Pruebas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cifradoss.Cifrados;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lol = Cesar.Encipher("Hola Mundo, ESTE ES MI PRIMER CIFRADO; FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAA", 5);
+            var lal = Cesar.Decipher(lol, 5);
+            var wow = new Ruta().Cipher("d", "r", lal, 3);
+            var waw = new Ruta().Cipher("r", "d", wow, 3);
+            var asd= new Zigzag().Encipher(5,lal);
+            var asdasd = new Zigzag().Uncipher(5,asd);
+
+        }
+    }
+}
