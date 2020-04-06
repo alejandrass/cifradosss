538d57efe4574a2773711e9025044abc83f0dedb
Alejandra Samayoa <ale.samayoa@outlook.es>
2020-04-05 19:33:08 -0600

    Initial commit

---
+++ DatosRecibidos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cifradoss
+{
+    public class DatosRecibidos
+    {
+        public string path { get; set; }
+        public string fileName { get; set; }
+        public int  rails { get; set; }
+        public int rows { get; set; }
+    }
+}
